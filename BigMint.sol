a7833abbe2ae76626fad52bb42cd11115958bd00
scorpnode <865672+scorpnode@users.noreply.github.com>
2021-10-17 19:15:41 +1100

    Update BigMint.sol

--- BigMint.sol
+++ BigMint.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.6.12;
 interface A { 
 function createProtonForSale(  address creator,  address receiver,  string memory tokenMetaUri,  uint256 annuityPercent,  uint256 royaltiesPercent,  uint256 salePrice) external returns (uint256 newTokenId);
 }
-
+//old contract address:0x806320473490779381583f38920CBf3328E558F3 Updated to allow Bag tracking.
 contract BigMint{
     
      address payable public owner;
@@ -11,17 +11,28 @@ contract BigMint{
         bool public isPaused=false;
         event mintSuccessful(uint256, uint32);
     mapping (uint256 => bool) public TokenIds;
+    mapping (uint32 =>uint256) public BagIds;
    constructor() public payable {
      owner = msg.sender;
+         uint32 protonID=835;     
+     for (uint i=0; i<35; i++) {
+           NFTcount++;
+      BagIds[NFTcount]=protonID;
+      TokenIds[protonID]=true;
+      protonID++;
+        }
+     
+
    }
     function mint_nft() public payable returns (uint256 tokenId){
-   A minter = A(0xe2a9b15E283456894246499Fb912CCe717f83319);
+      A minter = A(0xe2a9b15E283456894246499Fb912CCe717f83319);
   require(msg.value >= 100000000000000000000 || msg.sender==owner);
       require(NFTcount<10000 && isPaused==false);  
      NFTcount++;
       tokenId=minter.createProtonForSale(owner,msg.sender,string(abi.encodePacked('https://ipfs.io/ipfs/QmaVP2ka3cH3fvoRTZXoimQJu4DEpmZqmpnaLEk3wC22Dr/', uint2str(NFTcount), '.json'))
     ,0,300,0);
     TokenIds[tokenId]=true;
+    BagIds[NFTcount]=tokenId;
      emit mintSuccessful(tokenId,NFTcount);
     return tokenId;
     }
